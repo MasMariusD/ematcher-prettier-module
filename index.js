2a57201c71939f745b4fa2623c8ec48b69a8509d
Mas Doornik <mas@e-matcher.com>
2026-01-04 15:53:26 +0100

    Finished basic module setup, now testing with link

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Happy developing ✨')
+module.exports = {
+    // JS, CSS, HTML
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    printWidth: 100,
+
+    // PHP
+    phpVersion: '7.1',
+    braceStyle: '1tbs',
+
+    // Plugin for PHP
+    plugins: [require.resolve('@prettier/plugin-php')],
+
+    overrides: [
+    {
+      // Use PHP parser for .phtml files
+      files: '*.phtml',
+      options: {
+        parser: 'php',
+      },
+    },
+    {
+      files: ['*.html', '*.css'],
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
+}
